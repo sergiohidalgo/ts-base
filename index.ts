00faa822a9c902caafb872a01bbbdc14ebeb5aea
Sergio Hidalgo <shidalgo@acid.cl>
2022-09-20 19:13:47 -0300

    update example code

--- index.ts
+++ index.ts
@@ -1,26 +1,14 @@
 import {readFileSync, writeFile} from 'fs'
-import moment from 'moment'
-import slugify from 'slugify'
-import { v4 as uuidv4 } from 'uuid';
 
-const file = readFileSync('./days-list.csv', 'utf-8')
-const icsBase = readFileSync('./base-ics.ics', 'utf-8')
+// utils libs:
+// import moment from 'moment'
+// import slugify from 'slugify'
+// import { v4 as uuidv4 } from 'uuid';
+// import axios from 'axios';
 
-const formatCsv = (file: string) => file.split('\r\n').map((day: string) => {
-    const [name, date] = day.split(',')
-    const initDate = moment(date, 'DD-MM-YY').format('YYYYMMDD')
-    const endDate = moment(date, 'DD-MM-YY').add(1, 'day').format('YYYYMMDD')
-    return {name, initDate, endDate}
-})
+console.log('init script')
 
-const listWithNameAndDate = formatCsv(file);
+const file = readFileSync('./example.csv', 'utf-8')
+writeFile(`./result.csv`, file, cb => console.log(cb))
 
-listWithNameAndDate.forEach(({name, initDate, endDate}) => {
-    const ics = icsBase
-        .replace(/__EVENT-DATE__/g, initDate)
-        .replace(/__EVENT-NAME__/g, `💆🏻 Viernes libre ${name}`)
-        .replace(/__EVENT-END-DATE__/g, endDate)
-        .replace(/__UID__/g, uuidv4().toUpperCase())
-        .replace(/__UID-ALARM__/g, uuidv4().toUpperCase())
-    if (name) writeFile(`ics/${slugify(name, '-')}.ics`, ics, cb => {})
-})
+console.log('end script')
